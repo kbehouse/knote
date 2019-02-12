b2a7da0d547685f03b9cb7fdd80780cdb9aeebb1
kartik_mac <kbehouse@gmail.com>
2019-02-12 22:35:57 +0800

    update to kbehouse

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,22 +23,22 @@ const users = [
 const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://kbeanyn.github.io', // Your website URL
-  baseUrl: '/kbenote/', // Base URL for your project */
+  url: 'https://kbehosue.github.io', // Your website URL
+  baseUrl: '/knote/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'kbenote',
-  organizationName: 'kbeanyn',
+  projectName: 'knote',
+  organizationName: 'kbehouse',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs333'},
+    {doc: 'doc1', label: 'Docs'},
     {doc: 'doc4', label: 'API'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
