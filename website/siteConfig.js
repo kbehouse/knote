5552b0ff603283047fc47a7a9d263f09a00bf5a5
kartik_mac <kbehouse@gmail.com>
2019-02-13 16:52:34 +0800

    add DRL and react tab for testing

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,13 +1,3 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-// See https://docusaurus.io/docs/site-config for all the possible
-// site configuration options.
-
 // List of projects/orgs using your project for the users page.
 const users = [
   {
@@ -21,13 +11,15 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
+  title: 'KNote', // Title for your website.
+  tagline: 'My notebook for learning something',
   url: 'https://kbehosue.github.io', // Your website URL
   baseUrl: '/knote/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
+
+  docsSideNavCollapsible: true,
 
   // Used for publishing and more
   projectName: 'knote',
@@ -38,10 +30,10 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
-    {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
+    {doc: 'doc1', label: 'Docs_kbe'},
+    {doc: 'react-install-1', label: 'React'},
+    // {page: 'help', label: 'Help'},
+    // {blog: true, label: 'Blog'},
   ],
 
   // If you have users set above, you add it here:
@@ -57,20 +49,6 @@ const siteConfig = {
     primaryColor: '#2E8555',
     secondaryColor: '#205C3B',
   },
-
-  /* Custom fonts for website */
-  /*
-  fonts: {
-    myFont: [
-      "Times New Roman",
-      "Serif"
-    ],
-    myOtherFont: [
-      "-apple-system",
-      "system-ui"
-    ]
-  },
-  */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
   copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
@@ -92,12 +70,10 @@ const siteConfig = {
   ogImage: 'img/docusaurus.png',
   twitterImage: 'img/docusaurus.png',
 
-  // Show documentation's last contributor's name.
-  // enableUpdateBy: true,
-
   // Show documentation's last update time.
-  // enableUpdateTime: true,
+  enableUpdateTime: true,
 
+  editUrl: 'https://github.com/kbehouse/knote/edit/master/docs/',
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
   //   repoUrl: 'https://github.com/facebook/test-site',
